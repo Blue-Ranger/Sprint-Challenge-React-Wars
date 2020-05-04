caea72d452155ec384b85ba89b9e66d781659b49
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-05-04 00:01:27 -0400

    added some code not done

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,28 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+import DeeperComponent from "./DeeperComponent";
+
+const WrapperDiv = styled.div`
+  margin: auto;
+  width: 80%;
+  height: 80%;
+  text-align: center;
+  background-color: black;
+  color: white;
+`;
+
+const Character = (props) => {
+  return (
+    <WrapperDiv>
+      <h1>Name: {props.someData.name}</h1>
+      <h1>Birth Date: {props.someData.birth_year}</h1>
+      <h1>Gender: {props.someData.gender}</h1>
+      <h1>Height: {props.someData.height}</h1>
+      <h1>Hair Color: {props.someData.hair_color}</h1>
+
+      <DeeperComponent />
+    </WrapperDiv>
+  );
+};
+
+export default Character;
---
+++ starwars/src/components/DeeperComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const DeeperComponent = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default DeeperComponent;
